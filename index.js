0062f37ec49c790f105a89f65c23de8b98b15208
gangadharteli18 <gangadharteli27@gmail.com>
2026-03-27 07:54:16 +0530

    created bus booking system 1st commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const connection=mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'0323',
-    database:'testdb'
+    database:'busdb'
 })
 
 connection.connect((err)=>{
@@ -16,18 +16,57 @@ connection.connect((err)=>{
     }
     console.log("connection has been created")
 
-    const createTable=`create table student(
+    const createUserTable=`create table users(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30),
     email VARCHAR(30)
     )`
-    connection.execute(createTable,(err)=>{
+     const createBusTable=`create table buses(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    busNumber VARCHAR(10),
+    tatalSeats int,
+    avilableSeats int
+    )`
+     const createBookingTable=`create table bookings(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    seatNumber int
+    )`
+     const createPaymentsTable=`create table payments(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    amountPaid int,
+    paymentStatus VARCHAR(12)
+    )`
+    connection.execute(createUserTable,(err)=>{
         if(err){
             console.log(err)
             connection.end()
             return;
         }
-        console.log("table created")
+        console.log("user table created")
+    })
+     connection.execute(createBusTable,(err)=>{
+        if(err){
+            console.log(err)
+            connection.end()
+            return;
+        }
+        console.log("bus table created")
+    })
+     connection.execute(createBookingTable,(err)=>{
+        if(err){
+            console.log(err)
+            connection.end()
+            return;
+        }
+        console.log(" bookings table created")
+    })
+     connection.execute(createPaymentsTable,(err)=>{
+        if(err){
+            console.log(err)
+            connection.end()
+            return;
+        }
+        console.log("payment table created")
     })
 })
 
